2f05dcdacaa8b15e50bbeca699bf03d0856e4f12
Acocalypso <acocalypso@gmail.com>
2022-09-15 21:21:58 +0200

    create db file

---
+++ db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS `raid_receiver` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `raid_receiver`;
+
+CREATE TABLE IF NOT EXISTS `raids` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `latitude` float DEFAULT 0,
+  `longitude` float DEFAULT 0,
+  `level` int(11) DEFAULT NULL,
+  `pokemon_id` int(11) DEFAULT NULL,
+  `team_id` int(11) DEFAULT NULL,
+  `cp` int(11) DEFAULT NULL,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `evolution` int(11) DEFAULT NULL,
+  `spawn` int(11) DEFAULT NULL,
+  `move_1` int(11) DEFAULT NULL,
+  `move_2` int(11) DEFAULT NULL,
+  `gym_id` varchar(50) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `is_ex_raid_eligible` varchar(50) DEFAULT NULL,
+  `is_exclusive` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
